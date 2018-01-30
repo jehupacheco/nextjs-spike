877777788eb7876965a4a4edfcc1f5772a180cb2
Jehu Pacheco <jehu.pacheco@tektonlabs.com>
2018-01-30 15:42:26 -0500

    Global Styles. Fonts

---
+++ utils/fonts.js
@@ -0,0 +1,51 @@
+export default `
+  @font-face {
+    font-family: Ubuntu;
+    src: url('/static/fonts/Ubuntu-Regular.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: Ubuntu;
+    font-style: italic;
+    src: url('/static/fonts/Ubuntu-Italic.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: Ubuntu;
+    font-weight: 300;
+    src: url('/static/fonts/Ubuntu-Light.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: Ubuntu;
+    font-style: italic;
+    font-weight: 300;
+    src: url('/static/fonts/Ubuntu-LightItalic.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: Ubuntu;
+    font-weight: 500;
+    src: url('/static/fonts/Ubuntu-Medium.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: Ubuntu;
+    font-style: italic;
+    font-weight: 500;
+    src: url('/static/fonts/Ubuntu-MediumItalic.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: Ubuntu;
+    font-weight: 700;
+    src: url('/static/fonts/Ubuntu-Bold.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: Ubuntu;
+    font-style: italic;
+    font-weight: 700;
+    src: url('/static/fonts/Ubuntu-BoldItalic.ttf') format('truetype');
+  }
+`;
